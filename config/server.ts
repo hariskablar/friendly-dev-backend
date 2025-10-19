2e0959f877514b718b244e1a9f4c36caf220a694
hariskablar <125084282+hariskablar@users.noreply.github.com>
2025-10-19 16:37:29 +0200

    Revert "change on server.ts"
    This reverts commit a3324ffa8adb50a85926ce87b7ddd947cf7f0766.

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env(
-    'RENDER_EXTERNAL_URL',
-    'https://friendly-dev-backend-rexa.onrender.com'
-  ),
-  proxy: true, // 👈 this makes Render HTTPS work properly
   app: {
     keys: env.array('APP_KEYS'),
   },
